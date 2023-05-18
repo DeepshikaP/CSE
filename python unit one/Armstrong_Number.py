4f7dd4adaa840e62090d0a0240a7e1655dc91c50
Deepshika P <123289067+DeepshikaP@users.noreply.github.com>
2023-05-18 14:52:58 +0000

    CELSIUS TO FAHRENHEIT

--- python unit one/Armstrong_Number.py
+++ python unit one/Armstrong_Number.py
@@ -4,8 +4,8 @@ for i in range (s,e+1):
     n=0
     t=i
     while t>0 :
-      a=t%10
-      n=n+(a*a*a)
-      t=t//10
+        a=t%10
+        n=n+(a*a*a)
+        t=t//10
     if(i==n):
-          print(i)
+        print(i)
---
+++ python unit one/celcsius_to_fahrenheit.py
@@ -0,0 +1,4 @@
+print("CELSIUS             FAHRENHEIT")
+for i in range(0,101,10):
+    f=int(i*(9/5)+32)
+    print(" ",i,"                ",f)
