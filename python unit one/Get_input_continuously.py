83c218398263b79b27ea4c15ef6b3bd6daaece44
Deepshika P <123289067+DeepshikaP@users.noreply.github.com>
2023-05-18 16:22:05 +0000

    Get input untill we enter "-1" and see odd or not

--- python unit one/Get_input_continuously.py
+++ python unit one/Get_input_continuously.py
@@ -1,3 +1,11 @@
+e=0
+o=0
 n=1
 while n!=-1:
     n=int(input("enter a number : "))
+    if(n%2==0):
+        e=e+1
+    else:
+        o=o+1
+print("no.of odd numbers entered : ",o)
+print("no.of even numbers entered : ",e)
